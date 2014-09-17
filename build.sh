c4422650ca4496a098514f37029765e894bc8e9f
Christian Treppo <christian@treppo.org>
2014-09-17 18:33:33 +0200

    Remove tabs and trailing spaces in build

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-./node_modules/resume-cli/index.js export -t elegant index
+./node_modules/resume-cli/index.js export --theme elegant --format html index &&
+  expand -t 4 index.html | sed 's/ *$//' > tmp.html && mv tmp.html index.html
