bc5558c167bc31bedad7f698370b1313aa68e2dc
Christian Treppo <christian@treppo.org>
2014-09-17 19:13:34 +0200

    Use nicer theme

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-./node_modules/resume-cli/index.js export --theme elegant --format html index &&
+./node_modules/resume-cli/index.js export --theme classy --format html index &&
   expand -t 4 index.html | sed 's/ *$//' > tmp.html && mv tmp.html index.html
